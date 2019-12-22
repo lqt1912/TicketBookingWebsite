8368d50192dcb9822ca600ff97ace06886ae3a61
Thanh Quyen <17520963@gm.uit.edu.vn>
2019-12-22 11:53:33 +0700

    Add some UI for admin site
    UI for 5 mange screen and 2 add screen

---
+++ MovieTheater1/MovieTheater1/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieTheater1.Models;
+namespace MovieTheater1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // manage screen
+        public ActionResult QuanLyPhim()
+        {
+            return View(DataAccess.GetPhims());
+        }
+        public ActionResult QuanLyRap()
+        {
+            return View(DataAccess.GetRaps());
+        }
+        public ActionResult QuanLyKhuyenMai()
+        {
+            return View(DataAccess.GetKhuyenMai());
+        }
+        public ActionResult QuanLyThanhVien()
+        {
+            return View();
+        }
+        public ActionResult QuanLyThongTinChieu()
+        {
+           
+            return View(DataAccess.GetThongtinchieu());
+        }
+       
+        public ActionResult QuanLyVe()
+        {
+            return View();
+        }
+        // add screen
+        public ActionResult ThemFilm()
+        {
+            return View();
+        }
+        public ActionResult ThemKhuyenMai()
+        {
+            return View();
+        }
+
+    }
+}
